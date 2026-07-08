c9ab1c0c2bd3b342d019815740a617f195e6be8b
hasithaea <hasitha2k4@gmail.com>
2026-07-08 21:27:25 +0530

    before modifying into circular queue

---
+++ Queues/3/3.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#define SIZE 10
+
+typedef struct{
+    int size;
+    int front;
+    int rear;
+    int data[SIZE];
+}queue;
+
+typedef enum{
+    FALSE,
+    TRUE
+}boolean;
+
+void init(queue*q){
+    q->size=0;
+    q->front=0;
+    q->rear=-1;
+}
+
+boolean isEmpty(queue*q){
+    if(q->size==0)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+boolean isFull(queue*q){
+    if(q->size==SIZE)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+void enqueue(queue*q, data d){
+    if(isFull(q)){
+        printf("Queue is full\n");
+    }
+    else{
+        q->rear++;
+        q->data[q->rear]=d;
+        q->front++;
+    }
+}
+
+int dequeue(queue*q){
+    if(isEmpty(q)){
+        printf("Queue is Empty\n");
+        return -1;
+    }
+    else{
+        int a=q->data[q->front];
+        q->front++;
+        q->size--;
+        return a;
+    }
+}
+
+int getFront(queue*q){
+    if(isEmpty(q)){
+        printf("Queue is Empty\n");
+        return -1;
+    }
+    else{
+        return q->data[q->front];
+    }  
+}
+
+void display(queue*q){
+    int i=q->front;
+
+    printf("Queue: \n");
+
+    while(i<=q->rear){
+        printf("%d", q->data[i]);
+        i++;
+    }
+    printf("\n");
+}
