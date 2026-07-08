ad8dc885c197e1cdd18217fea77ab7c65e478cac
hasithaea <hasitha2k4@gmail.com>
2026-07-08 21:58:04 +0530

    after modifying into circular queue

--- Queues/3/3.c
+++ Queues/3/3.c
@@ -33,14 +33,21 @@ boolean isFull(queue*q){
         return FALSE;
 }
 
+int increment(int x)
+{
+    if(++x==SIZE)
+        x=0;
+    return x;
+}
+
 void enqueue(queue*q, data d){
     if(isFull(q)){
         printf("Queue is full\n");
     }
     else{
-        q->rear++;
+        q->rear= increment(q->rear);
         q->data[q->rear]=d;
-        q->front++;
+        q->size++;
     }
 }
 
@@ -49,9 +56,10 @@ int dequeue(queue*q){
         printf("Queue is Empty\n");
         return -1;
     }
-    else{
+    else
+    {
         int a=q->data[q->front];
-        q->front++;
+        q->front=increment(q->front);
         q->size--;
         return a;
     }
@@ -72,9 +80,46 @@ void display(queue*q){
 
     printf("Queue: \n");
 
-    while(i<=q->rear){
+    while(i!=q->rear){
         printf("%d", q->data[i]);
-        i++;
+        i=increment(i);
     }
+    printf("%d", q->data[q->rear]);
     printf("\n");
+}
+
+int main()
+{
+    queue q;
+    int i,n;
+
+    init(&q);
+
+    for(i=0; i<SIZE; i++)
+    {
+		printf("Enter element %d\n", i+1);
+		scanf("%d", &n);
+		enqueue(&q, n);
+	}
+
+    display(&q);
+
+    printf("\nDeleted Element: %d\n", dequeue(&q));
+	printf("\nDeleted Element: %d\n", dequeue(&q));
+	printf("\nDeleted Element: %d\n", dequeue(&q));
+
+    display(&q);
+
+    printf("\n");
+
+    for(i=0; i<SIZE;i++)
+    {
+	    printf("Enter element %d\n", i+1);
+		scanf("%d", &n);
+		enqueue(&q, n);
+	}
+
+    display(&q);
+
+    return 0;
 }
