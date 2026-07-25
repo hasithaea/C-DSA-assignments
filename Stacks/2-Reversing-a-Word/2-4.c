f99725acb285c8d36572b911d20ba9c51bca42f5
hasithaea <hasitha2k4@gmail.com>
2026-07-26 03:02:39 +0530

    improve code structure

--- Stacks/2-Reversing-a-Word/2-4.c
+++ Stacks/2-Reversing-a-Word/2-4.c
@@ -2,24 +2,64 @@
 #include <string.h>
 #define SIZE 20
 
-typedef struct 
-{
+typedef struct {
     int top;
     char data[SIZE];
 }stack;
 
-void init(stack*);
-void push(stack*, char);
-char pop(stack*);
-char top(stack*);
-int isEmpty(stack*);
-int isFull(stack*);
-void display(stack*);
+void init(stack*s) {
+    s->top = -1;
+}
+
+int isEmpty(stack*s) {
+    return s->top==-1;
+}
+
+int isFull(stack*s) {
+    return s->top==SIZE-1;
+}
+
+char top(stack*s) {
+    return s->data[s->top];
+}
+
+void push(stack*s, char x) {
+    if(isFull(s)) {
+        printf("Stack full\n");
+        return;
+    }
+    
+    s->top++;
+    s->data[s->top] = x;
+}
+
+char pop(stack*s) {
+    if(isEmpty(s)) {
+        printf("Stack empty\n");
+        return -1;
+    }
+    
+    char a = s->data[s->top];
+    s->top--;
+    return a;
+}
+
+void display(stack*s) {
+    int i = s->top;
+    printf("\nDisplay stack\n====================\n");
+
+    while(i>=0) {
+        printf("%c ", s->data[i]);
+        i--;
+    }
+
+    printf("\n");
+}
 
 int main()
 {
     stack s;
-    int j=0;
+    int j = 0;
     char word[SIZE];
 
     init(&s);
@@ -30,8 +70,7 @@ int main()
     printf("Enter a word : ");
     scanf("%s", word);
 
-    while(j<=strlen(word))
-    {
+    while(j<=strlen(word)) {
         push(&s,word[j]);
         j++;
     }
@@ -45,64 +84,15 @@ int main()
 }
 
 
-void init(stack*s)
-{
-    s->top=-1;
-}
 
-int isEmpty(stack*s)
-{
-    return s->top==-1;
-}
 
-int isFull(stack*s)
-{
-    return s->top==SIZE-1;
-}
 
-char top(stack*s)
-{
-    return s->data[s->top];
-}
 
-void push(stack*s, char x)
-{
-    if(isFull(s))
-    {
-        printf("Stack full\n");
-    }
-    else
-    {
-        s->top++;
-        s->data[s->top]=x;
-    }
-}
 
-char pop(stack*s)
-{
-    if(isEmpty(s))
-    {
-        printf("Stack empty\n");
-        return -1;
-    }
-    else
-    {
-        char a=s->data[s->top];
-        s->top--;
-        return a;
-    }
-}
 
-void display(stack*s)
-{
-    int i=s->top;
-    printf("\nDisplay stack\n====================\n");
 
-    while(i>=0)
-    {
-        printf("%c ", s->data[i]);
-        i--;
-    }
 
-    printf("\n");
-}
+
+
+
+
